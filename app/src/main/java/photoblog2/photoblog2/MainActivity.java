64b2bb984ee79c2e6b483e3f57c273efdc4544ea
DayMoriwaki <moriwaki.nodai@gmail.com>
2018-12-30 17:36:34 +0900

    Part2_07:20

--- app/src/main/java/photoblog2/photoblog2/MainActivity.java
+++ app/src/main/java/photoblog2/photoblog2/MainActivity.java
@@ -1,7 +1,11 @@
 package photoblog2.photoblog2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
 
     }
 }
