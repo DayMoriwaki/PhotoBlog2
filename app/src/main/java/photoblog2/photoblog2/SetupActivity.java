6aa65228633e75a995c691b7968c8c6a4742f36f
DayMoriwaki <moriwaki.nodai@gmail.com>
2019-01-02 10:31:13 +0900

    Part5_終了

--- app/src/main/java/photoblog2/photoblog2/SetupActivity.java
+++ app/src/main/java/photoblog2/photoblog2/SetupActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -11,14 +12,36 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
 
     private CircleImageView setupImage;
+    private Uri mainImageURI = null;
+
+    private EditText setupName;
+    private Button setupBtn;
+    private ProgressBar setupProgress;
+
+    private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -30,7 +53,56 @@ public class SetupActivity extends AppCompatActivity {
         setSupportActionBar(setupToolbar);
         getSupportActionBar().setTitle("Account Setup");
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         setupImage = findViewById(R.id.setup_image);
+        setupName = findViewById(R.id.setup_name);
+        setupBtn = findViewById(R.id.setup_btn);
+        setupProgress = findViewById(R.id.setup_progress);
+
+        setupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user_name = setupName.getText().toString();
+
+                if(!TextUtils.isEmpty(user_name) && mainImageURI != null){
+
+                    String user_id = firebaseAuth.getCurrentUser().getUid();
+                    setupProgress.setVisibility(View.VISIBLE);
+
+                    StorageReference image_path = storageReference.child("profile_images").child(user_id + ".jpg");
+                    image_path.putFile(mainImageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>(){
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task){
+
+                            Uri download_uri;
+
+                            if(task.isSuccessful()){
+
+                                download_uri = task.getResult().getUploadSessionUri();
+                                Toast.makeText(SetupActivity.this, "The Image is Uploaded", Toast.LENGTH_LONG).show();
+
+
+                            }else{
+
+                                String error = task.getException().getMessage();
+                                Toast.makeText(SetupActivity.this, "Error :" + error, Toast.LENGTH_LONG).show();
+
+                            }
+
+                            setupProgress.setVisibility(View.INVISIBLE);
+
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+
 
         setupImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +117,11 @@ public class SetupActivity extends AppCompatActivity {
 
                     }else{
 
-                        Toast.makeText(SetupActivity.this, "You already have permission", Toast.LENGTH_LONG).show();
+//                        Toast.makeText(SetupActivity.this, "You already have permission", Toast.LENGTH_LONG).show();
+                        CropImage.activity()
+                                .setGuidelines(CropImageView.Guidelines.ON)
+                                .setAspectRatio(1,1)
+                                .start(SetupActivity.this);
 
                     }
 
@@ -64,5 +140,27 @@ public class SetupActivity extends AppCompatActivity {
 //
 //
 //    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                mainImageURI = result.getUri();
+
+                setupImage.setImageURI(mainImageURI);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+            }
+        }
+
+    }
+
+
 
 }
